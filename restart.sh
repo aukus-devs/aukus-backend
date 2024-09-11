f784c33b3ab1d00354426010208daa8c0afb6474
olegsvs <oleg.texet@gmail.com>
2024-09-11 04:32:40 +0300

    Save log

--- restart.sh
+++ restart.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
 ps -ef | grep 'only_api_app' | awk '{print $2}'  | xargs -r kill -9
 sleep 2
-nohup python3.10 only_api_app.py > backend.log &
+nohup python3.10 only_api_app.py >> backend.log &
