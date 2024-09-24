d5f1be7c688d5da16b1f2953cfe0704d0eaf74ed
olegsvs <oleg.texet@gmail.com>
2024-09-24 10:30:21 +0300

    Set file-based session

--- only_api_app.py
+++ only_api_app.py
@@ -4,7 +4,8 @@ from dotenv import load_dotenv
 import os
 import logging
 from datetime import datetime, timedelta
-from flask import Flask, session
+from flask import Flask
+from flask_session.__init__ import Session
 from routes.api.login.login import auth_bp
 from routes.api.player.player import player_bp
 from routes.api.canvas.canvas import canvas_bp
@@ -21,16 +22,15 @@ app = Flask(__name__)
 logger = app.logger
 
 
-@app.before_request
-def before_request():
-    app.before_request_funcs[None].remove(before_request)
-    session.permanent = True
-    app.permanent_session_lifetime = timedelta(days=30)
-
-
 def create_app():
     app.secret_key = config.SESSION_SECRET
     app.config['UPLOAD_FOLDER'] = config.UPLOAD_FOLDER
+    app.config["SESSION_PERMANENT"] = True
+    app.config["SESSION_TYPE"] = "filesystem"
+    app.config['SESSION_FILE_THRESHOLD'] = 250
+    app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=30)
+
+    Session(app)
 
     app.register_blueprint(auth_bp)
     app.register_blueprint(player_bp)
