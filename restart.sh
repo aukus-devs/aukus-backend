49f616e0a6e9fc3327877988284004ff8c6f2667
olegsvs <oleg.texet@gmail.com>
2024-09-28 21:56:54 +0300

    Fix CI

--- restart.sh
+++ restart.sh
@@ -3,4 +3,4 @@ ps -ef | grep 'only_api_app' | awk '{print $2}'  | xargs -r kill -9
 ps -ef | grep 'only_api_app' | awk '{print $2}'  | xargs -r kill -9
 sleep 2
 #nohup python3.10 only_api_app.py >> backend.log &
-nohup gunicorn 'only_api_app:create_app()'  --log-level debug --workers 1 --bind 127.0.0.1:5000  >> backend.log &
+nohup gunicorn 'only_api_app:create_app()'  --log-level debug --workers 1 --bind 127.0.0.1:5000  >> ./backend.log 2>&1 &
