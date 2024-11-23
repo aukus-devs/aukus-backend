806e81766ea882bbf4e97e271b70b8697d0f862c
Maxim Velesyuk <max.velesyuk@gmail.com>
2024-11-23 02:45:03 +0100

    Reformat file

--- background_jobs.py
+++ background_jobs.py
@@ -14,7 +14,7 @@ twitch_headers = {
     "Authorization": os.getenv("TWITCH_BEARER_TOKEN"),
 }
 
-logging.basicConfig(level = logging.INFO)
+logging.basicConfig(level=logging.INFO)
 
 
 def reset_finished_players():
@@ -27,30 +27,50 @@ def refresh_stream_statuses():
         db = DatabaseClient()
         players = db.get_all_players()
         for player in players:
-            #logging.info(str(player))
-            if player["twitch_stream_link"]: #twitch link exists
-                #logging.info("Start twitch check for " + player["username"] + ", URL: " + player["twitch_stream_link"])
-                url = "https://api.twitch.tv/helix/streams?user_login=" + player["twitch_stream_link"].rsplit('/', 1)[1]
+            # logging.info(str(player))
+            if player["twitch_stream_link"]:  # twitch link exists
+                # logging.info("Start twitch check for " + player["username"] + ", URL: " + player["twitch_stream_link"])
+                url = (
+                    "https://api.twitch.tv/helix/streams?user_login="
+                    + player["twitch_stream_link"].rsplit("/", 1)[1]
+                )
                 response = requests.get(url, headers=twitch_headers, timeout=15)
                 data = response.json()["data"]
-                #logging.info(data)
+                # logging.info(data)
                 if len(data) != 0 and data[0]["type"] == "live":
                     stream = data[0]
-                    if stream["game_name"] != player["player_stream_current_category"] or player["player_is_online"] == False: # comparing category with DB
-                        db.update_stream_status(player_id=player["id"], is_online=True, category=stream["game_name"])
+                    if (
+                        stream["game_name"] != player["player_stream_current_category"]
+                        or player["player_is_online"] == False
+                    ):  # comparing category with DB
+                        db.update_stream_status(
+                            player_id=player["id"],
+                            is_online=True,
+                            category=stream["game_name"],
+                        )
                 else:
-                    if player["player_is_online"] == True: # is online in DB?
+                    if player["player_is_online"] == True:  # is online in DB?
                         db.update_stream_status(player_id=player["id"], is_online=False)
-            elif player["vk_stream_link"]: #vkplay link exists
-                #logging.info("Start vkplay check for " + player["username"] + ", URL: " + player["vk_stream_link"])
+            elif player["vk_stream_link"]:  # vkplay link exists
+                # logging.info("Start vkplay check for " + player["username"] + ", URL: " + player["vk_stream_link"])
                 vkplay_page = requests.get(player["vk_stream_link"], timeout=30)
                 content = html.fromstring(vkplay_page.content)
-                category_xpath = content.xpath('/html/body/div[1]/div/div[2]/div[2]/div/div[3]/div[1]/div[1]/div/div[2]/div[1]/div[3]/a')
+                category_xpath = content.xpath(
+                    "/html/body/div[1]/div/div[2]/div[2]/div/div[3]/div[1]/div[1]/div/div[2]/div[1]/div[3]/a"
+                )
                 if len(category_xpath) != 0:
-                    if category_xpath[0].text != player["player_stream_current_category"] or player["player_is_online"] == False: # comparing category with DB
-                        db.update_stream_status(player_id=player["id"], is_online=True, category=category_xpath[0].text)
+                    if (
+                        category_xpath[0].text
+                        != player["player_stream_current_category"]
+                        or player["player_is_online"] == False
+                    ):  # comparing category with DB
+                        db.update_stream_status(
+                            player_id=player["id"],
+                            is_online=True,
+                            category=category_xpath[0].text,
+                        )
                 else:
-                    if player["player_is_online"] == True: # is online in DB?
+                    if player["player_is_online"] == True:  # is online in DB?
                         db.update_stream_status(player_id=player["id"], is_online=False)
     except Exception as e:
         logging.error("Stream check failed for " + player["username"] + ",: " + str(e))
