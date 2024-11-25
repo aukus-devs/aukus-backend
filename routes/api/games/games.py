2455ee36a845e032f4fb391da7b2576f8cc39e2d
olegsvs <oleg.texet@gmail.com>
2024-11-25 14:22:18 +0300

    Games API: disable auth

--- routes/api/games/games.py
+++ routes/api/games/games.py
@@ -8,18 +8,7 @@ games_bp = Blueprint("games", __name__)
 db = GamesDatabaseClient()
 
 
-def login_required(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if "username" not in session and "role" not in session:
-            return jsonify({"error": f"Auth required"}), 401
-        return f(*args, **kwargs)
-
-    return decorated_function
-
-
 @games_bp.route("/api/games", methods=["GET"])
-@login_required
 def search_games():
     title = request.args.get("title")
     if not title:
