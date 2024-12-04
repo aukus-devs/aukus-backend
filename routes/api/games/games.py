17a435a560b813719cf3e3a7b2544847a518ab46
olegsvs <oleg.texet@gmail.com>
2024-12-04 14:32:11 +0300

    Search games: improve IGDB filtering

--- routes/api/games/games.py
+++ routes/api/games/games.py
@@ -25,17 +25,20 @@ def search_games():
         return jsonify({"error": f"Missing required field: title"}), 400
     igdb_token = db.get_igdb_token()["igdb_token"]
     headers = {"Client-ID": IGDB_CLIENT_ID, "Authorization": "Bearer " + igdb_token}
-    response = igdb_session.post("https://api.igdb.com/v4/games", headers=headers, data='search "' + title + '"; fields id, name, cover.image_id;', timeout=1)
-    games = None
-    if response.ok and "name" in response.text and len(response.text) > 2:
-        games = []
-        games_json = json.loads(response.content.decode('utf-8'))
-        for game in games_json:
-            games.append({
-                "id": game["id"],
-                "gameName": game["name"],
-                "box_art_url": "https://images.igdb.com/igdb/image/upload/t_cover_big/" + game["cover"]["image_id"] + ".jpg" if "cover" in game else None
-            })
-    else:
-        games = games_db.search_games(title)
+    payload = ('fields id,name,cover.image_id; limit 50; where name ~ *"' + title.lower() + '"*;').encode('utf-8')
+    games = []
+    try:
+        response = igdb_session.post("https://api.igdb.com/v4/games", headers=headers, data=payload, timeout=1)
+        if response.ok and "name" in response.text and len(response.text) > 2:
+            games_json = json.loads(response.content.decode('utf-8'))
+            for game in games_json:
+                games.append({
+                    "id": game["id"],
+                    "gameName": game["name"],
+                    "box_art_url": "https://images.igdb.com/igdb/image/upload/t_cover_big/" + game["cover"]["image_id"] + ".jpg" if "cover" in game else None
+                })
+        else:
+            games = games_db.search_games(title)
+    except:
+        games.extend(games_db.search_games(title.lower()))
     return jsonify({"games": games})
