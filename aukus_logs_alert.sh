864a4a16c606e9c4c76e96626d1df0abccc377c4
olegsvs <oleg.texet@gmail.com>
2025-01-13 06:38:00 +0300

    Add v2025 to deploy

--- aukus_logs_alert.sh
+++ aukus_logs_alert.sh
@@ -6,7 +6,6 @@ while read line ; do
         then
              msg=${line//\"/\\\"} ; \
              #echo "test $msg"
-             curl -x $SOCKS_5_PROXY -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"$msg\"}" $DISCORD_WEBHOOK
-             curl -s -X POST -d "message_thread_id=18&chat_id=-1002471795184_18&text=${msg}" $TG_API
+             curl -x $SOCKS_5_PROXY -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"v2025: $msg\"}" $DISCORD_WEBHOOK
         fi
 done
