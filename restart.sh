de0cf2e278d53741b5f1f03c5c1bfa2867ac11f3
olegsvs <oleg.texet@gmail.com>
2025-01-13 06:57:36 +0300

    Disable bg jobs

--- restart.sh
+++ restart.sh
@@ -10,5 +10,5 @@ d=$(date +%Y-%m-%d-%H-%M-%S)
 mv backend.log old_logs/backend_$d.log
 #nohup python3.10 only_api_app.py >> backend.log &
 nohup gunicorn 'v2025_only_api_app:create_app()'  --log-level debug --workers 4 --bind 127.0.0.1:5200  >> ./backend.log 2>&1 &
-nohup python v2025_background_jobs.py  >> ./backend.log 2>&1 &
+#nohup python v2025_background_jobs.py  >> ./backend.log 2>&1 &
 nohup sh aukus_2025_logs_alert.sh >/dev/null 2>&1 &
