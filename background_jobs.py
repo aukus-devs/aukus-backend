7e89b4cc6cc553b939b62c26cc5c0763465d3cb4
olegsvs <oleg.texet@gmail.com>
2025-01-22 13:54:16 +0300

    Disable kick.com checks

--- background_jobs.py
+++ background_jobs.py
@@ -126,7 +126,8 @@ def refresh_stream_statuses():
                         + ",: "
                         + str(e)
                     )
-            elif player["disabled_kick_stream_link"]:  # disabled
+            elif player["kick_stream_link"]:
+                return
                 # logging.info("Start kick check for " + player["username"] + ", URL: " + player["kick_stream_link"])
                 try:
                     url = "http://localhost:20080/v1"  # cloudflare bypass proxy
