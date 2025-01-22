a4242d1f80bec0b5524088ce8b0d84a295d5560a
olegsvs <oleg.texet@gmail.com>
2025-01-22 12:31:25 +0300

    Dockerization

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,5 @@ WORKDIR /app
 COPY ./requirements.txt requirements.txt
 RUN pip install --no-cache-dir --upgrade -r requirements.txt
 COPY . .
-CMD ["gunicorn", "--bind", "0.0.0.0:5300", "v2025_only_api_app:create_app()"]
+RUN chmod +x ./restart.sh
+ENTRYPOINT ./restart.sh
--- restart.sh
+++ restart.sh
@@ -1,14 +1,3 @@
-#!/usr/bin/sh
-ps -ef | grep 'aukus_2025_logs_alert.sh' | awk '{print $2}'  | xargs -r kill -9
-#sleep 2
-ps -ef | grep 'v2025_only_api_app' | awk '{print $2}'  | xargs -r kill -9
-ps -ef | grep 'v2025_only_api_app' | awk '{print $2}'  | xargs -r kill -9
-ps -ef | grep 'v2025_background_jobs' | awk '{print $2}'  | xargs -r kill -9
-#sleep 2
-mkdir old_logs
-d=$(date +%Y-%m-%d-%H-%M-%S)
-mv backend.log old_logs/backend_$d.log
-#nohup python3.10 only_api_app.py >> backend.log &
-nohup gunicorn 'v2025_only_api_app:create_app()'  --log-level debug --workers 4 --bind 127.0.0.1:5200  >> ./backend.log 2>&1 &
-#nohup python v2025_background_jobs.py  >> ./backend.log 2>&1 &
-nohup sh aukus_2025_logs_alert.sh >/dev/null 2>&1 &
+#docker run --env-file ../.env_2025_prod --net=host -p 5200:5200 -it $(docker build -t olegsvs/aukus:2025-prod -q .)
+#docker run -d -e AUKUS_BACKEND_PORT=5200 --env-file ../.env_2025_prod --net=host --name aukus_2025_prod -it olegsvs/aukus:2025-prod
+gunicorn 'only_api_app:create_app()'  --log-level debug --workers 4 --bind 0.0.0.0:${AUKUS_BACKEND_PORT}
