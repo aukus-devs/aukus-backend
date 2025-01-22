71b831a9d37d71b7938e820e4ad446963076661f
olegsvs <oleg.texet@gmail.com>
2025-01-22 13:03:47 +0300

    Docker: fix logs

--- restart.sh
+++ restart.sh
@@ -2,4 +2,4 @@
 #docker run -d -e AUKUS_BACKEND_PORT=5200 --env-file ../.env_2025_prod --net=host --name aukus_2025_prod -it olegsvs/aukus:2025-prod
 nohup sh aukus_logs_alert.sh >/dev/null 2>&1 &
 nohup python background_jobs.py  >> ./backend.log 2>&1 &
-gunicorn 'only_api_app:create_app()'  --log-level debug --workers 4 --bind 0.0.0.0:${AUKUS_BACKEND_PORT} | tee backend.log
+gunicorn 'only_api_app:create_app()'  --access-logfile backend.log --error-logfile backend.log --capture-output --log-level debug --workers 4 --bind 0.0.0.0:${AUKUS_BACKEND_PORT} | tail -F ./backend.log
