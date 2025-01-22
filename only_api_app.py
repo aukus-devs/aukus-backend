6e2604c6ab06d9a5ac54a336b9003c4014201586
olegsvs <oleg.texet@gmail.com>
2025-01-22 14:14:48 +0300

    Flask: store sessions in mysql

--- only_api_app.py
+++ only_api_app.py
@@ -3,14 +3,21 @@ from datetime import datetime, timedelta
 from flask import Flask, session, request
 from flask_session.__init__ import Session
 from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
 from routes.api.login.login import auth_bp, init_bcrypt
 from routes.api.player.player import player_bp
 from routes.api.canvas.canvas import canvas_bp
 from routes.api.games.games import games_bp
 from routes.api.rules.rules import rules_bp
+from dotenv import load_dotenv
+import os
 import config
 
 logging.basicConfig(level=logging.DEBUG)
+load_dotenv()
+MYSQL_LOGIN = os.getenv("MYSQL_LOGIN")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_HOST = os.getenv("MYSQL_HOST")
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 
@@ -18,8 +25,10 @@ bcrypt = Bcrypt(app)
 def create_app():
     app.secret_key = config.SESSION_SECRET
     app.config["SESSION_PERMANENT"] = True
-    app.config["SESSION_TYPE"] = "filesystem"
-    app.config["SESSION_FILE_THRESHOLD"] = 250
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{MYSQL_LOGIN}:{MYSQL_PASSWORD}@{MYSQL_HOST}/aukus_2025_db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config["SESSION_TYPE"] = "sqlalchemy"
+    app.config['SESSION_SQLALCHEMY'] = SQLAlchemy(app)
     app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(days=30)
     app.config["DEBUG"] = True
     app.config["JSON_AS_ASCII"] = False
