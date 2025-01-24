b9e3f21b84592d19d785d895cd4cd4bc788daef2
olegsvs <oleg.texet@gmail.com>
2025-01-24 14:48:15 +0300

    Show host in logs alerts

--- aukus_logs_alert.sh
+++ aukus_logs_alert.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/sh
+curl -x $AUKUS_SOCKS5_PROXY -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"v2025 ${AUKUS_HOST}: Booted\"}" $LOGS_DISCORD_WEBHOOK
 tail -Fn0 backend.log | \
 while read line ; do
         echo "$line" | grep -i "error" | grep -v "errorlog" | grep -v "See wait_timeout and interactive_timeout"
@@ -6,6 +7,6 @@ while read line ; do
         then
              msg=${line//\"/\\\"} ; \
              #echo "test $msg"
-             curl -x $AUKUS_SOCKS5_PROXY -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"v2025: $msg\"}" $LOGS_DISCORD_WEBHOOK
+             curl -x $AUKUS_SOCKS5_PROXY -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"v2025 ${AUKUS_HOST}: $msg\"}" $LOGS_DISCORD_WEBHOOK
         fi
 done
