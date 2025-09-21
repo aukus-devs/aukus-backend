6a7ff802cd2d1a3a30467f5e63577e161fc5ce86
Maxim Velesyuk <max.velesyuk@gmail.com>
2025-09-21 13:44:39 +0200

    Specify type for db_setting variable

--- src/utils/common.py
+++ src/utils/common.py
@@ -1,5 +1,5 @@
-from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.sql import select
 
 
 from src.db.db_models import EventSettings
@@ -11,7 +11,7 @@ from src.enums import (
 async def get_event_setting(db: AsyncSession, setting: EventSetting) -> str | None:
     query = select(EventSettings).where(EventSettings.key_name == setting.value)
     result = await db.execute(query)
-    db_setting = result.scalar_one_or_none()
+    db_setting: EventSettings | None = result.scalar_one_or_none()
     if db_setting:
         return db_setting.value
     return None
