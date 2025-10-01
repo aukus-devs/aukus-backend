9c009510ba36e221f15573e3b355e2e14bba50ef
Maxim Velesyuk <max.velesyuk@gmail.com>
2025-10-02 00:33:46 +0200

    Set EventLab API URL based on environment
    The EventLab API URL is now set to "http://localhost:8300" when running locally and
    "https://api.eventlab.dev" otherwise.

--- src/config.py
+++ src/config.py
@@ -10,7 +10,7 @@ DATABASE_URL = "mysql+asyncmy://root:pass@127.0.0.1:3306/aukus4" if IS_LOCAL els
 
 TOKEN_SECRET_KEY = os.getenv("SECRET_KEY", "your-secret-key")
 
-EVENTLAB_API_URL = os.getenv("EVENTLAB_API_URL", "http://localhost:8300")
+EVENTLAB_API_URL = "http://localhost:8300" if IS_LOCAL else "https://api.eventlab.dev"
 
 
 def setup_logging():
