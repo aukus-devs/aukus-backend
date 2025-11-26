24691f274be5b9acebd28670fe8f97318b2ec646
Maxim Velesyuk <max.velesyuk@gmail.com>
2025-11-26 11:30:29 +0100

    Require COMPLETED move type for rate-10 and rate-0 achievements

--- src/api/player/utils.py
+++ src/api/player/utils.py
@@ -301,9 +301,15 @@ def check_achievement_completion(achievement: Achievement, moves: list[PlayerMov
             ]
             return len(sheikh_moves) >= 3
         case "rate-10":
-            return last_move.item_rating == 10
+            return (
+                last_move.item_rating == 10
+                and last_move.type == PlayerMoveType.COMPLETED.value
+            )
         case "rate-0":
-            return last_move.item_rating == 0
+            return (
+                last_move.item_rating == 0
+                and last_move.type == PlayerMoveType.COMPLETED.value
+            )
         case "complete-easy":
             return (
                 last_move.type == PlayerMoveType.COMPLETED.value
